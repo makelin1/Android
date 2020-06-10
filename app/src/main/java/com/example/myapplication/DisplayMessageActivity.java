3cceec265df37a9ce3220d8aed00215dcf549175
mkl <mkl@qq.com>
2020-06-10 16:49:39 +0800

    访问webservice

--- app/src/main/java/com/example/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myapplication/DisplayMessageActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.TextView;
 
@@ -22,7 +23,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
     //需要调用的方法名(获得本天气预报Web Services支持的洲、国内外省份和城市信息)
     private static final String GETSUPPORTPROVINCE = "sayHello";
     //WSDL文档中给定的URL
-    private static final String WSDL = "http://webService.demo.example.com/webservice/aaa";
+    private static final String WSDL = "http://10.100.2.45:9999/test/webservice/aaa?wsdl";
 
 
     @Override
@@ -34,30 +35,26 @@ public class DisplayMessageActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.textView3);
         textView.setText(message);
-    }
 
-    public String getHii(View view) throws IOException, XmlPullParserException {
-        System.out.println(111111);
-        //实例化SOAP对象，指定命名空间和调用的方法
-        SoapObject soapObject = new SoapObject(TARGETNAMESPACE,GETSUPPORTPROVINCE);
-        soapObject.addProperty("name","mkl");
-        //使用SOAP1.1协议创建Envelop对象
-        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-        //设置与.Dot提供的Web Service服务保持良好的兼容性
-        envelope.dotNet = true;
-        //将初始化的soapObject放入（序列化）使用SOAP1.1协议创建Envelop对象中
-        envelope.setOutputSoapObject(soapObject);
 
-        System.out.println(2222);
-        //构建传输对象，指定URL
-        HttpTransportSE httpTransportSE = new HttpTransportSE(WSDL);
-        httpTransportSE.call(TARGETNAMESPACE+GETSUPPORTPROVINCE,envelope);
-        SoapObject result = (SoapObject) envelope.getResponse();
-        System.out.println(result);
-        return "";
     }
 
     public void getHi(View view) throws IOException, XmlPullParserException {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    getHii();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (XmlPullParserException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public String getHii() throws IOException, XmlPullParserException {
         System.out.println(111111);
         //实例化SOAP对象，指定命名空间和调用的方法
         SoapObject soapObject = new SoapObject(TARGETNAMESPACE,GETSUPPORTPROVINCE);
@@ -72,7 +69,37 @@ public class DisplayMessageActivity extends AppCompatActivity {
         System.out.println(2222);
         //构建传输对象，指定URL
         HttpTransportSE httpTransportSE = new HttpTransportSE(WSDL);
+        System.out.println(333);
         httpTransportSE.call(TARGETNAMESPACE+GETSUPPORTPROVINCE,envelope);
+        System.out.println(4444);
+//        SoapObject result = (SoapObject) envelope.getResponse();
+        Object result = (Object) envelope.getResponse();
+        System.out.println(result);
+
+        String message = result.toString();
+        TextView textView = findViewById(R.id.textView4);
+        textView.setText(message);
+        return "";
+    }
+
+    public void getHi111(View view) throws IOException, XmlPullParserException {
+        System.out.println(111111);
+        //实例化SOAP对象，指定命名空间和调用的方法
+        SoapObject soapObject = new SoapObject(TARGETNAMESPACE,GETSUPPORTPROVINCE);
+        soapObject.addProperty("name","mkl");
+        //使用SOAP1.1协议创建Envelop对象
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+        //设置与.Dot提供的Web Service服务保持良好的兼容性
+        envelope.dotNet = true;
+        //将初始化的soapObject放入（序列化）使用SOAP1.1协议创建Envelop对象中
+        envelope.setOutputSoapObject(soapObject);
+
+        System.out.println(2222);
+        //构建传输对象，指定URL
+        HttpTransportSE httpTransportSE = new HttpTransportSE(WSDL);
+        System.out.println(333);
+        httpTransportSE.call(" ",envelope);
+        System.out.println(4444);
         SoapObject result = (SoapObject) envelope.getResponse();
         System.out.println(result);
 
